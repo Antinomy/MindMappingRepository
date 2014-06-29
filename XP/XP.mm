1473e9b2162e3b0146a69541cf9c148b705cb6fd
antinomy <antinomy.net@126.com>
2014-06-29 15:03:30 +0800

    New MindMapping

--- XP/XP.mm
+++ XP/XP.mm
@@ -1,84 +1,81 @@
-<map version="1.0.0">
+<map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node COLOR="#ff6633" CREATED="1398920019933" ID="ID_881269574" MODIFIED="1398920057589" TEXT="XP">
-<edge COLOR="#b4b4b4"/>
-<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="20"/>
+<node COLOR="#000000" CREATED="1404018389249" ID="ID_1577456031" MODIFIED="1404018550439" TEXT="XP">
+<font NAME="SansSerif" SIZE="20"/>
 <hook NAME="accessories/plugins/AutomaticLayout.properties"/>
-<node COLOR="#0033ff" CREATED="1398920022902" ID="ID_1721104641" MODIFIED="1398920057480" POSITION="left" TEXT="Simple Design">
-<edge COLOR="#ebd95f" STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="18"/>
+<node COLOR="#0033ff" CREATED="1404018389249" ID="ID_136462639" MODIFIED="1404018536309" POSITION="left" TEXT="Simple Design">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
 </node>
-<node COLOR="#0033ff" CREATED="1398920022902" ID="ID_258864095" MODIFIED="1398920057495" POSITION="right" TEXT="The Planning Game">
-<edge COLOR="#7aa3e5" STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1398920022902" ID="ID_1034281499" MODIFIED="1398920057511" TEXT="Scrum">
-<edge COLOR="#81aae5" STYLE="bezier" WIDTH="thin"/>
-<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="16"/>
+<node COLOR="#0033ff" CREATED="1404018389250" ID="ID_1782534626" MODIFIED="1404018536308" POSITION="left" TEXT="The Planning Game">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1404018389250" ID="ID_1163051652" MODIFIED="1404018536308" TEXT="Scrum">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
 </node>
-<node COLOR="#00b439" CREATED="1398920022902" ID="ID_451285116" MODIFIED="1398920057511" TEXT="Kanban">
-<edge COLOR="#7b9ce5" STYLE="bezier" WIDTH="thin"/>
-<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="16"/>
+<node COLOR="#00b439" CREATED="1404018389250" ID="ID_663954717" MODIFIED="1404018536308" TEXT="Kanban">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1398920022902" ID="ID_371892607" MODIFIED="1398920057511" POSITION="left" TEXT="Testing">
-<edge COLOR="#efa670" STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1398920022902" ID="ID_1974275362" MODIFIED="1398920057527" TEXT="TDD">
-<edge COLOR="#ed9d67" STYLE="bezier" WIDTH="thin"/>
-<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="16"/>
-<node COLOR="#990000" CREATED="1398920022902" ID="ID_22810594" MODIFIED="1398920057527" TEXT="ATDD">
-<edge COLOR="#ee9c65"/>
-<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="14"/>
+<node COLOR="#0033ff" CREATED="1404018389250" ID="ID_108893369" MODIFIED="1404018536306" POSITION="left" TEXT="Testing">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1404018389250" ID="ID_585326250" MODIFIED="1404018536306" TEXT="TDD">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1404018389250" ID="ID_1673412835" MODIFIED="1404018536305" TEXT="ATDD">
+<font NAME="SansSerif" SIZE="14"/>
 </node>
-<node COLOR="#990000" CREATED="1398920022902" ID="ID_1432410078" MODIFIED="1398920057527" TEXT="BDD">
-<edge COLOR="#ecab6b"/>
-<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="14"/>
+<node COLOR="#990000" CREATED="1404018389250" ID="ID_1108835635" MODIFIED="1404018536305" TEXT="BDD">
+<font NAME="SansSerif" SIZE="14"/>
 </node>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1398920022902" ID="ID_1319844068" MODIFIED="1398920057542" POSITION="right" TEXT="Small Releases">
-<edge COLOR="#67d7c4" STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1398920022902" ID="ID_25962192" MODIFIED="1398920057558" TEXT="Continues Delivery">
-<edge COLOR="#64d7c1" STYLE="bezier" WIDTH="thin"/>
-<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="16"/>
+<node COLOR="#0033ff" CREATED="1404018389251" ID="ID_1685108409" MODIFIED="1404018536305" POSITION="left" TEXT="Small Releases">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1404018389251" ID="ID_865087830" MODIFIED="1404018536304" TEXT="Continues Delivery">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1398920022902" ID="ID_1689022955" MODIFIED="1398920057558" POSITION="left" TEXT="Refactoring">
-<edge COLOR="#e68782" STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="18"/>
+<node COLOR="#0033ff" CREATED="1404018389251" ID="ID_56402999" MODIFIED="1404018536304" POSITION="left" TEXT="Refactoring">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
 </node>
-<node COLOR="#0033ff" CREATED="1398920022902" ID="ID_956332037" MODIFIED="1398920057558" POSITION="right" TEXT="Metaphor">
-<edge COLOR="#9ed56b" STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1398920022902" ID="ID_1588777364" MODIFIED="1398920057558" TEXT="DDD">
-<edge COLOR="#9cd86d" STYLE="bezier" WIDTH="thin"/>
-<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="16"/>
-</node>
-</node>
-<node COLOR="#0033ff" CREATED="1398920022902" ID="ID_1365190497" MODIFIED="1398920057573" POSITION="left" TEXT="Pair Programming">
-<edge COLOR="#e096e9" STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="18"/>
-</node>
-<node COLOR="#0033ff" CREATED="1398920022902" ID="ID_1111794207" MODIFIED="1398920057589" POSITION="left" TEXT="Collective Ownership">
-<edge COLOR="#988ee3" STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="18"/>
-</node>
-<node COLOR="#0033ff" CREATED="1398920022902" ID="ID_230479498" MODIFIED="1398920057589" POSITION="right" TEXT="40 Hour week">
-<edge COLOR="#7aa3e5" STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1398920022902" ID="ID_1691116526" MODIFIED="1398920057589" TEXT="Sustainable Rate">
-<edge COLOR="#80a9e4" STYLE="bezier" WIDTH="thin"/>
-<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="16"/>
+<node COLOR="#0033ff" CREATED="1404018389251" ID="ID_1139170123" MODIFIED="1404018536303" POSITION="right" TEXT="Metaphor">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1404018389251" ID="ID_125399486" MODIFIED="1404018536303" TEXT="DDD">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1398920022902" ID="ID_1005846292" MODIFIED="1398920057589" POSITION="left" TEXT="Coding Standards">
-<edge COLOR="#9ed56b" STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="18"/>
+<node COLOR="#0033ff" CREATED="1404018389251" ID="ID_923044738" MODIFIED="1404018536303" POSITION="right" TEXT="Pair Programming">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
 </node>
-<node COLOR="#0033ff" CREATED="1398920022902" ID="ID_598864874" MODIFIED="1398920057589" POSITION="right" TEXT="On Site Customer">
-<edge COLOR="#67d7c4" STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="18"/>
+<node COLOR="#0033ff" CREATED="1404018389251" ID="ID_794687501" MODIFIED="1404018536303" POSITION="right" TEXT="Collective Ownership">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1404018389251" ID="ID_50415271" MODIFIED="1404018536302" POSITION="right" TEXT="40 Hour week">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1404018389251" ID="ID_207131438" MODIFIED="1404018536302" TEXT="Sustainable Rate">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1404018389251" ID="ID_1252801968" MODIFIED="1404018536301" POSITION="right" TEXT="Coding Standards">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1404018389252" ID="ID_1552553496" MODIFIED="1404018536299" POSITION="right" TEXT="On Site Customer">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
 </node>
 </node>
 </map>
