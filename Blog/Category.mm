c24d5c7cb10312759cb269a82f7db3cd6e28a755
antinomy <antinomy.net@126.com>
2014-07-06 15:15:20 +0800

    New MindMapping for blog

---
+++ Blog/Category.mm
@@ -0,0 +1,23 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1404614200873" ID="ID_1986967982" MODIFIED="1404614243561" STYLE="bubble" TEXT="Mr.A">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1404614200873" HGAP="22" ID="ID_978533084" LINK="/category/Tao.html" MODIFIED="1404614913840" POSITION="left" STYLE="bubble" TEXT="&#x9053;" VSHIFT="-24">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1404614200874" ID="ID_523567400" LINK="/category/Art.html" MODIFIED="1404614925414" POSITION="left" STYLE="bubble" TEXT="&#x5320;" VSHIFT="11">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1404614200874" HGAP="21" ID="ID_319986317" LINK="/category/Tech.html" MODIFIED="1404614819971" POSITION="right" STYLE="bubble" TEXT="&#x672f;" VSHIFT="-23">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1404614200874" HGAP="23" ID="ID_1477115009" LINK="/category/Other.html" MODIFIED="1404614895371" POSITION="right" STYLE="bubble" TEXT="&#x6742;" VSHIFT="11">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</map>
