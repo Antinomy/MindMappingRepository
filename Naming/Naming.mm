ba612dbbf86d3a3094525cc9f8e4154894304731
antinomy <antinomy.net@126.com>
2014-07-12 16:37:15 +0800

    IDDD_1

---
+++ Naming/Naming.mm
@@ -0,0 +1,48 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1404632872229" ID="ID_1256450916" MODIFIED="1404632885886" TEXT="Naming">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1404632872229" HGAP="19" ID="ID_861146278" MODIFIED="1404632901435" POSITION="left" TEXT="Camel(&#x9a7c;&#x5cf0;&#x5f0f;)" VSHIFT="-10">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1404632872229" ID="ID_646878027" MODIFIED="1404632905155" TEXT="lowerCamel(&#x5c0f;&#x9a7c;&#x5cf0;)" VSHIFT="-27">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1404632872230" ID="ID_398952195" MODIFIED="1404632885840" TEXT="UpperCamel(&#x5927;&#x9a7c;&#x5cf0;)">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node BACKGROUND_COLOR="#eeee00" COLOR="#990000" CREATED="1404632872230" ID="ID_1231756542" MODIFIED="1404632885860" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>&#x4e5f;&#x53eb;Pascal (&#x5e15;&#x65af;&#x5361;)</p></body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1404632872231" HGAP="30" ID="ID_360844545" MODIFIED="1404632917227" POSITION="right" TEXT="Snake(&#x86c7;&#x5f0f;)" VSHIFT="-77">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1404632872231" ID="ID_883815977" MODIFIED="1404632885882" TEXT="snake_case">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1404632872231" HGAP="26" ID="ID_253384417" MODIFIED="1404632903259" POSITION="right" TEXT="Hungarian(&#x5308;&#x7259;&#x5229;)" VSHIFT="50">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1404632872231" ID="ID_253767724" MODIFIED="1404632885886" TEXT="c_Hungarian">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
