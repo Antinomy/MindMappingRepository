73a4eeadc1bf754c4c8a9c9cee00706dc8c4af37
antinomy <antinomy.net@126.com>
2014-08-26 20:18:14 +0800

    0824-IDDD-ALL

--- IDDD/IDDD.mm
+++ IDDD/IDDD.mm
@@ -1,6 +1,6 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node BACKGROUND_COLOR="#00bfff" COLOR="#000000" CREATED="1405822652168" ID="ID_318591736" MODIFIED="1405822701625" TEXT="IDDD">
+<node BACKGROUND_COLOR="#00bfff" COLOR="#000000" CREATED="1408868961212" ID="ID_1057246951" MODIFIED="1408869058504" STYLE="bubble" TEXT="IDDD">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -12,63 +12,63 @@
   </body>
 </html>
 </richcontent>
-<font NAME="SansSerif" SIZE="20"/>
+<font NAME="Serif" SIZE="20"/>
 <hook NAME="accessories/plugins/AutomaticLayout.properties"/>
-<node COLOR="#0033ff" CREATED="1405822652168" ID="ID_678400341" LINK="./IDDD_1.org" MODIFIED="1405822676429" POSITION="left" TEXT="&#x4e00;&#x3001;&#x5165;&#x95e8;">
+<node COLOR="#0033ff" CREATED="1408868961212" ID="ID_239288612" LINK="./IDDD_1.org" MODIFIED="1408869054452" POSITION="left" STYLE="bubble" TEXT="&#x4e00;&#x3001;&#x5165;&#x95e8;">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="SansSerif" SIZE="18"/>
+<font NAME="Serif" SIZE="18"/>
 </node>
-<node COLOR="#0033ff" CREATED="1405822652169" ID="ID_373642030" LINK="./IDDD_2.org" MODIFIED="1405822676432" POSITION="left" TEXT="&#x4e8c;&#x3001;&#x9886;&#x57df;&#x3001;&#x5b50;&#x57df;&#x548c;&#x9650;&#x754c;&#x4e0a;&#x4e0b;&#x6587;">
+<node COLOR="#0033ff" CREATED="1408868961213" ID="ID_752115817" LINK="./IDDD_2.org" MODIFIED="1408869054450" POSITION="left" STYLE="bubble" TEXT="&#x4e8c;&#x3001;&#x9886;&#x57df;&#x3001;&#x5b50;&#x57df;&#x548c;&#x9650;&#x754c;&#x4e0a;&#x4e0b;&#x6587;">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="SansSerif" SIZE="18"/>
+<font NAME="Serif" SIZE="18"/>
 </node>
-<node COLOR="#0033ff" CREATED="1405822652169" ID="ID_928590332" LINK="./IDDD_3.org" MODIFIED="1405822676435" POSITION="left" TEXT="&#x4e09;&#x3001;&#x4e0a;&#x4e0b;&#x6587;&#x6620;&#x5c04;&#x56fe;">
+<node COLOR="#0033ff" CREATED="1408868961213" ID="ID_1314054367" LINK="./IDDD_3.org" MODIFIED="1408869054448" POSITION="left" STYLE="bubble" TEXT="&#x4e09;&#x3001;&#x4e0a;&#x4e0b;&#x6587;&#x6620;&#x5c04;&#x56fe;">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="SansSerif" SIZE="18"/>
+<font NAME="Serif" SIZE="18"/>
 </node>
-<node COLOR="#0033ff" CREATED="1405822652170" ID="ID_1209192309" LINK="./IDDD_4.org" MODIFIED="1405822676438" POSITION="left" TEXT="&#x56db;&#x3001;&#x67b6;&#x6784;">
+<node COLOR="#0033ff" CREATED="1408868961213" ID="ID_810805516" LINK="./IDDD_4.org" MODIFIED="1408869054446" POSITION="left" STYLE="bubble" TEXT="&#x56db;&#x3001;&#x67b6;&#x6784;">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="SansSerif" SIZE="18"/>
+<font NAME="Serif" SIZE="18"/>
 </node>
-<node COLOR="#0033ff" CREATED="1405822652170" ID="ID_879244708" LINK="./IDDD_5.org" MODIFIED="1405822676441" POSITION="left" TEXT="&#x4e94;&#x3001;&#x5b9e;&#x4f53;">
+<node COLOR="#0033ff" CREATED="1408868961214" ID="ID_1921739179" LINK="./IDDD_5.org" MODIFIED="1408869054445" POSITION="left" STYLE="bubble" TEXT="&#x4e94;&#x3001;&#x5b9e;&#x4f53;">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="SansSerif" SIZE="18"/>
+<font NAME="Serif" SIZE="18"/>
 </node>
-<node COLOR="#0033ff" CREATED="1405822652170" ID="ID_1649752563" LINK="./IDDD_6.org" MODIFIED="1405822676444" POSITION="left" TEXT="&#x516d;&#x3001;&#x503c;&#x5bf9;&#x8c61;">
+<node COLOR="#0033ff" CREATED="1408868961214" ID="ID_1027517260" LINK="./IDDD_6.org" MODIFIED="1408869054443" POSITION="left" STYLE="bubble" TEXT="&#x516d;&#x3001;&#x503c;&#x5bf9;&#x8c61;">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="SansSerif" SIZE="18"/>
+<font NAME="Serif" SIZE="18"/>
 </node>
-<node COLOR="#0033ff" CREATED="1405822652170" ID="ID_1186648200" LINK="./IDDD_7.org" MODIFIED="1405822676447" POSITION="left" TEXT="&#x4e03;&#x3001;&#x9886;&#x57df;&#x670d;&#x52a1;">
+<node COLOR="#0033ff" CREATED="1408868961214" ID="ID_1183875808" LINK="./IDDD_7.org" MODIFIED="1408869054442" POSITION="left" STYLE="bubble" TEXT="&#x4e03;&#x3001;&#x9886;&#x57df;&#x670d;&#x52a1;">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="SansSerif" SIZE="18"/>
+<font NAME="Serif" SIZE="18"/>
 </node>
-<node COLOR="#0033ff" CREATED="1405822652170" ID="ID_1043500969" LINK="./IDDD_8.org" MODIFIED="1405822676449" POSITION="right" TEXT="&#x516b;&#x3001;&#x9886;&#x57df;&#x4e8b;&#x4ef6;">
+<node COLOR="#0033ff" CREATED="1408868961214" ID="ID_804747703" LINK="./IDDD_8.org" MODIFIED="1408869054441" POSITION="right" STYLE="bubble" TEXT="&#x516b;&#x3001;&#x9886;&#x57df;&#x4e8b;&#x4ef6;">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="SansSerif" SIZE="18"/>
+<font NAME="Serif" SIZE="18"/>
 </node>
-<node COLOR="#0033ff" CREATED="1405822652170" ID="ID_91315803" LINK="./IDDD_9.org" MODIFIED="1405822676452" POSITION="right" TEXT="&#x4e5d;&#x3001;&#x6a21;&#x5757;">
+<node COLOR="#0033ff" CREATED="1408868961214" ID="ID_1796755678" LINK="./IDDD_9.org" MODIFIED="1408869054440" POSITION="right" STYLE="bubble" TEXT="&#x4e5d;&#x3001;&#x6a21;&#x5757;">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="SansSerif" SIZE="18"/>
+<font NAME="Serif" SIZE="18"/>
 </node>
-<node COLOR="#0033ff" CREATED="1405822652171" ID="ID_1277156368" LINK="./IDDD_10.org" MODIFIED="1405822676454" POSITION="right" TEXT="&#x5341;&#x3001;&#x805a;&#x5408;">
+<node COLOR="#0033ff" CREATED="1408868961214" ID="ID_398060175" LINK="./IDDD_10.org" MODIFIED="1408869054438" POSITION="right" STYLE="bubble" TEXT="&#x5341;&#x3001;&#x805a;&#x5408;">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="SansSerif" SIZE="18"/>
+<font NAME="Serif" SIZE="18"/>
 </node>
-<node COLOR="#0033ff" CREATED="1405822652171" ID="ID_657770823" LINK="./IDDD_11.org" MODIFIED="1405822676457" POSITION="right" TEXT="&#x5341;&#x4e00;&#x3001;&#x5de5;&#x5382;">
+<node COLOR="#0033ff" CREATED="1408868961214" ID="ID_1986855526" LINK="./IDDD_11.org" MODIFIED="1408869054437" POSITION="right" STYLE="bubble" TEXT="&#x5341;&#x4e00;&#x3001;&#x5de5;&#x5382;">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="SansSerif" SIZE="18"/>
+<font NAME="Serif" SIZE="18"/>
 </node>
-<node COLOR="#0033ff" CREATED="1405822652171" ID="ID_829988682" LINK="./IDDD_12.org" MODIFIED="1405822676459" POSITION="right" TEXT="&#x5341;&#x4e8c;&#x3001;&#x8d44;&#x6e90;&#x5e93;">
+<node COLOR="#0033ff" CREATED="1408868961214" ID="ID_1854408031" LINK="./IDDD_12.org" MODIFIED="1408869054435" POSITION="right" STYLE="bubble" TEXT="&#x5341;&#x4e8c;&#x3001;&#x8d44;&#x6e90;&#x5e93;">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="SansSerif" SIZE="18"/>
+<font NAME="Serif" SIZE="18"/>
 </node>
-<node COLOR="#0033ff" CREATED="1405822652171" ID="ID_609657107" LINK="./IDDD_13.org" MODIFIED="1405822676462" POSITION="right" TEXT="&#x5341;&#x4e09;&#x3001;&#x96c6;&#x6210;&#x9650;&#x754c;&#x4e0a;&#x4e0b;&#x6587;">
+<node COLOR="#0033ff" CREATED="1408868961215" ID="ID_397681176" LINK="./IDDD_13.org" MODIFIED="1408869054433" POSITION="right" STYLE="bubble" TEXT="&#x5341;&#x4e09;&#x3001;&#x96c6;&#x6210;&#x9650;&#x754c;&#x4e0a;&#x4e0b;&#x6587;">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="SansSerif" SIZE="18"/>
+<font NAME="Serif" SIZE="18"/>
 </node>
-<node COLOR="#0033ff" CREATED="1405822652171" ID="ID_1959541149" LINK="./IDDD_14.org" MODIFIED="1405822676464" POSITION="right" TEXT="&#x5341;&#x56db;&#x3001;&#x5e94;&#x7528;&#x7a0b;&#x5e8f;">
+<node COLOR="#0033ff" CREATED="1408868961215" ID="ID_1816738095" LINK="./IDDD_14.org" MODIFIED="1408869054430" POSITION="right" STYLE="bubble" TEXT="&#x5341;&#x56db;&#x3001;&#x5e94;&#x7528;&#x7a0b;&#x5e8f;">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="SansSerif" SIZE="18"/>
+<font NAME="Serif" SIZE="18"/>
 </node>
 </node>
 </map>
