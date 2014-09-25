394e227c3db093f0b5ee17b88617b4e773128999
antinomy <antinomy.net@126.com>
2014-09-25 22:29:27 +0800

    2014-09-25-MindMapping-ChoiceOnTools

---
+++ MindMapping/ChoiceOnTools.mm
@@ -0,0 +1,165 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node text="&#x5de5;&#x5177;&#x7684;&#x9009;&#x62e9;">
+<node text="&#x7eb8;&#x7b14;" position="left">
+<node text="&#x4f18;&#x70b9;">
+<node style="bubble" background_color="#eeee00">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>&#x4e0d;&#x4f1a;&#x88ab;&#x5de5;&#x5177;&#x6240;&#x9650;&#x5236;&#xff0c;&#x601d;&#x7ef4;&#x65e0;&#x963b;</p></body>
+</html>
+</richcontent>
+</node>
+</node>
+<node text="&#x7f3a;&#x70b9;">
+<node style="bubble" background_color="#eeee00">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>&#x96be;&#x4ee5;&#x643a;&#x5e26;&#x548c;&#x5206;&#x4eab;</p></body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node text="&#x624b;&#x673a;&#xff1a;App" position="left">
+<node text="&#x4f18;&#x70b9;">
+<node style="bubble" background_color="#eeee00">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>&#x968f;&#x65f6;&#x968f;&#x5730;&#x8bb0;&#x5f55;&#x601d;&#x7ef4;&#xff0c;&#x6613;&#x643a;&#x5e26;&#x548c;&#x5206;&#x4eab;</p></body>
+</html>
+</richcontent>
+</node>
+</node>
+<node text="&#x7f3a;&#x70b9;">
+<node style="bubble" background_color="#eeee00">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>&#x5c4f;&#x5e55;&#x6709;&#x9650;&#xff0c;&#x601d;&#x7ef4;&#x963b;&#x529b;&#x5927;</p></body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node text="PC Website">
+<node text="&#x4f18;&#x70b9;">
+<node style="bubble" background_color="#eeee00">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>&#x65e0;&#x9700;&#x5ba2;&#x6237;&#x7aef;&#xff0c;&#x6253;&#x5f00;&#x7f51;&#x9875;&#x5373;&#x53ef;&#xff0c;&#x4fbf;&#x4e8e;&#x5206;&#x4eab;</p></body>
+</html>
+</richcontent>
+</node>
+</node>
+<node text="&#x7f3a;&#x70b9;">
+<node style="bubble" background_color="#eeee00">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>&#x53d7;&#x9650;&#x5236;&#x4e8e;&#x7f51;&#x7edc;&#x73af;&#x5883;&#xff0c;&#x601d;&#x7ef4;&#x963b;&#x529b;&#x5927;</p></body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node text="PC Client&#xff1a;FreeMind">
+<node text="&#x4f18;&#x70b9;">
+<node style="bubble" background_color="#eeee00">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>&#x4e0d;&#x53d7;&#x9650;&#x4e8e;&#x7f51;&#x7edc;&#x73af;&#x5883;&#xff0c;&#x601d;&#x7ef4;&#x963b;&#x529b;&#x5c0f;</p></body>
+</html>
+</richcontent>
+</node>
+</node>
+<node text="&#x7f3a;&#x70b9;">
+<node style="bubble" background_color="#eeee00">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>&#x96be;&#x4ee5;&#x7248;&#x672c;&#x63a7;&#x5236;&#xff0c;&#x5206;&#x4eab;&#x4e0d;&#x65b9;&#x4fbf;</p></body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node text="&#x6700;&#x7ec8;&#x65b9;&#x6848;&#xff1a; Org text">
+<node text="&#x4f18;&#x70b9;">
+<node style="bubble" background_color="#eeee00">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>&#x7eaf;&#x6587;&#x672c;&#x683c;&#x5f0f;&#xff0c;&#x7248;&#x672c;&#x63a7;&#x5236;&#x53cb;&#x597d;<br />&#x5236;&#x4f5c;&#x7b80;&#x5355;&#xff0c;&#x601d;&#x7ef4;&#x963b;&#x529b;&#x6700;&#x5c0f;&#x3002;<br />&#x53ef;&#x5bfc;&#x51fa;MM&#x683c;&#x5f0f;&#xff0c;&#x901a;&#x8fc7;&#x7b2c;&#x4e09;&#x65b9;&#x7f8e;&#x5316;</p></body>
+</html>
+</richcontent>
+</node>
+</node>
+<node text="&#x7f3a;&#x70b9;">
+<node style="bubble" background_color="#eeee00">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>&#x591a;&#x4e86;&#x4e00;&#x5c42;&#x4e2d;&#x95f4;&#x5c42;&#xff0c;&#x7565;&#x590d;&#x6742;</p></body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</map>
