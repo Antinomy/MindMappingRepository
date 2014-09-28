70c3ddc8026c9f7255fafdfbc795259bbb5c8701
antinomy <antinomy.net@126.com>
2014-09-28 13:01:43 +0800

    2014-09-28-OpenSourceLicence

---
+++ OpenSource/OpenSourceLicence.mm
@@ -0,0 +1,85 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1411879493878" ID="ID_669444531" MODIFIED="1411879604186" STYLE="bubble" TEXT="&#x5f00;&#x6e90;&#x8bb8;&#x53ef;&#x8bc1;">
+<font BOLD="true" NAME="Serif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1411879493878" ID="ID_1059779446" MODIFIED="1411879604184" POSITION="right" STYLE="bubble" TEXT="&#x5141;&#x8bb8;&#x95ed;&#x6e90;&#xff1f;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<node COLOR="#00b439" CREATED="1411879493878" HGAP="6" ID="ID_825745865" MODIFIED="1411879604178" STYLE="bubble" TEXT="&#x662f;" VSHIFT="-24">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Serif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1411879493878" ID="ID_619796560" MODIFIED="1411879604179" STYLE="bubble" TEXT="&#x5fc5;&#x987b;&#x5305;&#x62ec;&#x539f;&#x7248;&#x6743;&#xff1f;">
+<font NAME="Serif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1411879493878" ID="ID_1457457638" MODIFIED="1411879604180" STYLE="bubble" TEXT="&#x662f;">
+<font NAME="Serif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1411879493878" ID="ID_923689479" MODIFIED="1411879604181" STYLE="bubble" TEXT="Apache">
+<font NAME="Serif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1411879493878" ID="ID_1499471822" MODIFIED="1411879604181" STYLE="bubble" TEXT="&#x5426;">
+<font NAME="Serif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1411879493878" ID="ID_214957076" MODIFIED="1411879604181" STYLE="bubble" TEXT="&#x53ef;&#x884d;&#x751f;&#x5e7f;&#x544a;&#xff1f;">
+<font NAME="Serif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1411879493878" ID="ID_1110021100" MODIFIED="1411879604182" STYLE="bubble" TEXT="&#x662f;">
+<font NAME="Serif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1411879493878" ID="ID_1345799370" MODIFIED="1411879604183" STYLE="bubble" TEXT="MIT">
+<font NAME="Serif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1411879493878" ID="ID_28709124" MODIFIED="1411879604183" STYLE="bubble" TEXT="&#x5426;">
+<font NAME="Serif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1411879493879" ID="ID_370060589" MODIFIED="1411879604183" STYLE="bubble" TEXT="BSD">
+<font NAME="Serif" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1411879493879" HGAP="5" ID="ID_1554774310" MODIFIED="1411879604163" STYLE="bubble" TEXT="&#x5426;" VSHIFT="52">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Serif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1411879493879" HGAP="31" ID="ID_505915236" MODIFIED="1411879604164" STYLE="bubble" TEXT="&#x91c7;&#x7528;&#x540c;&#x6837;&#x7684;&#x8bb8;&#x53ef;&#x8bc1;&#xff1f;" VSHIFT="8">
+<font NAME="Serif" SIZE="14"/>
+<node BACKGROUND_COLOR="#eeee00" COLOR="#111111" CREATED="1411879493879" ID="ID_596489413" MODIFIED="1411879604165" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>&#x65b0;&#x589e;&#x4ee3;&#x7801;</p></body>
+</html></richcontent>
+<font NAME="Serif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1411879493885" ID="ID_494201893" MODIFIED="1411879604165" STYLE="bubble" TEXT="&#x662f;">
+<font NAME="Serif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1411879493885" ID="ID_1704138902" MODIFIED="1411879604166" STYLE="bubble" TEXT="GPL">
+<font NAME="Serif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1411879493885" ID="ID_1517054871" MODIFIED="1411879604166" STYLE="bubble" TEXT="&#x5426;">
+<font NAME="Serif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1411879493886" ID="ID_753442053" MODIFIED="1411879604167" STYLE="bubble" TEXT="&#x4fee;&#x6539;&#x5904;&#x9700;&#x63d0;&#x4f9b;&#x8bf4;&#x660e;&#x6587;&#x6863;&#xff1f;">
+<font NAME="Serif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1411879493886" ID="ID_1432486780" MODIFIED="1411879604167" STYLE="bubble" TEXT="&#x662f;">
+<font NAME="Serif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1411879493886" HGAP="23" ID="ID_1427499964" MODIFIED="1411879604167" STYLE="bubble" TEXT="Mozilla" VSHIFT="-3">
+<font NAME="Serif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1411879493886" ID="ID_504356938" MODIFIED="1411879604167" STYLE="bubble" TEXT="&#x5426;">
+<font NAME="Serif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1411879493886" HGAP="25" ID="ID_278061555" MODIFIED="1411879604168" STYLE="bubble" TEXT="LGPL" VSHIFT="8">
+<font NAME="Serif" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
