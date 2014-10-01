65dda084e2b61d56731983bf51a003eaf0a18765
antinomy <antinomy.net@126.com>
2014-10-01 23:52:16 +0800

    2014-10-01-TimeOfBigData

---
+++ BigData/TimeOfBigData.mm
@@ -0,0 +1,378 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1412177772061" ID="ID_1521656165" MODIFIED="1412177798695" STYLE="bubble" TEXT="&#x5927;&#x6570;&#x636e;&#x65f6;&#x4ee3;">
+<font BOLD="true" NAME="Serif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1412177772061" ID="ID_1878986499" MODIFIED="1412177798692" POSITION="left" STYLE="bubble" TEXT="&#x601d;&#x7ef4;&#x53d8;&#x9769;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font BOLD="true" NAME="Serif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1412177772062" ID="ID_1773533651" MODIFIED="1412177798690" STYLE="bubble" TEXT="&#x66f4;&#x591a;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="Serif" SIZE="16"/>
+<node BACKGROUND_COLOR="#eeee00" COLOR="#990000" CREATED="1412177772062" ID="ID_440002582" MODIFIED="1412177798691" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>&#x4e0d;&#x662f;&#x968f;&#x673a;&#x6837;&#x672c;&#xff0c;&#x800c;&#x662f;&#x5168;&#x4f53;&#x6570;&#x636e;</p></body>
+</html></richcontent>
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1412177772063" ID="ID_76368568" MODIFIED="1412177798691" STYLE="bubble" TEXT="&#x8ba9;&#x6570;&#x636e;&#x201c;&#x53d1;&#x58f0;&#x201d;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1412177772063" ID="ID_1746114367" MODIFIED="1412177798692" STYLE="bubble" TEXT="&#x5c0f;&#x6570;&#x636e;&#x65f6;&#x4ee3;&#x7684;&#x968f;&#x673a;&#x91c7;&#x6837;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+<node BACKGROUND_COLOR="#eeee00" COLOR="#111111" CREATED="1412177772063" ID="ID_1408555333" MODIFIED="1412177798692" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>&#x6700;&#x5c11;&#x7684;&#x4fe1;&#x606f;&#x6570;&#x636e;&#x83b7;&#x5f97;&#x6700;&#x591a;&#x7684;&#x4fe1;&#x606f;&#x3002;</p></body>
+</html></richcontent>
+<font BOLD="true" NAME="Serif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1412177772064" ID="ID_1100377383" MODIFIED="1412177798692" STYLE="bubble" TEXT="&#x5168;&#x6570;&#x636e;&#x6a21;&#x5f0f;&#xff1a;&#x6837;&#x672c;=&#x603b;&#x4f53;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1412177772064" HGAP="29" ID="ID_1644559914" MODIFIED="1412177807081" STYLE="bubble" TEXT="&#x66f4;&#x6742;" VSHIFT="26">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="Serif" SIZE="16"/>
+<node BACKGROUND_COLOR="#eeee00" COLOR="#990000" CREATED="1412177772064" ID="ID_1659328367" MODIFIED="1412177798686" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>&#x4e0d;&#x662f;&#x7cbe;&#x786e;&#x6027;&#xff0c;&#x800c;&#x662f;&#x6df7;&#x6742;&#x6027;</p></body>
+</html></richcontent>
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1412177772064" ID="ID_806087858" MODIFIED="1412177798686" STYLE="bubble" TEXT="&#x5141;&#x8bb8;&#x4e0d;&#x7cbe;&#x786e;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1412177772064" ID="ID_1085673719" MODIFIED="1412177798687" STYLE="bubble" TEXT="&#x5927;&#x6570;&#x636e;&#x7684;&#x7b80;&#x5355;&#x7b97;&#x6cd5;&#x6bd4;&#x5c0f;&#x6570;&#x636e;&#x7684;&#x590d;&#x6742;&#x7b97;&#x6cd5;&#x66f4;&#x6709;&#x6548;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1412177772068" ID="ID_784732916" MODIFIED="1412177798687" STYLE="bubble" TEXT="&#x7eb7;&#x7e41;&#x7684;&#x6570;&#x636e;&#x8d8a;&#x5927;&#x8d8a;&#x597d;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1412177772068" ID="ID_1528936510" MODIFIED="1412177798687" STYLE="bubble" TEXT="&#x6df7;&#x4e71;&#x662f;&#x4e00;&#x79cd;&#x6807;&#x51c6;&#x9014;&#x5f84;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+<node BACKGROUND_COLOR="#eeee00" COLOR="#111111" CREATED="1412177772068" ID="ID_797570442" MODIFIED="1412177798687" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>&#x4e0d;&#x8be5;&#x7aed;&#x529b;&#x907f;&#x514d;</p></body>
+</html></richcontent>
+<font BOLD="true" NAME="Serif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1412177772069" ID="ID_1837644494" MODIFIED="1412177798688" STYLE="bubble" TEXT="&#x65b0;&#x7684;&#x6570;&#x636e;&#x5e93;&#x8bbe;&#x8ba1;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+<node BACKGROUND_COLOR="#eeee00" COLOR="#111111" CREATED="1412177772069" ID="ID_525401222" MODIFIED="1412177798688" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>SQL&#x5df2;&#x50f5;&#x5316;&#xff0c;NoSQL&#x5d1b;&#x8d77;</p></body>
+</html></richcontent>
+<font BOLD="true" NAME="Serif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1412177772069" HGAP="22" ID="ID_363438024" MODIFIED="1412177810560" STYLE="bubble" TEXT="&#x66f4;&#x597d;" VSHIFT="40">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="Serif" SIZE="16"/>
+<node BACKGROUND_COLOR="#eeee00" COLOR="#990000" CREATED="1412177772069" ID="ID_1273590861" MODIFIED="1412177798679" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>&#x4e0d;&#x662f;&#x56e0;&#x679c;&#x5173;&#x7cfb;&#xff0c;&#x800c;&#x662f;&#x76f8;&#x5173;&#x5173;&#x7cfb;</p></body>
+</html></richcontent>
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1412177772072" ID="ID_957946822" MODIFIED="1412177798679" STYLE="bubble" TEXT="&#x5173;&#x8054;&#x7269;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+<node BACKGROUND_COLOR="#eeee00" COLOR="#111111" CREATED="1412177772072" ID="ID_1947087459" MODIFIED="1412177798680" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>&#x9884;&#x6d4b;&#x7684;&#x5173;&#x952e;</p></body>
+</html></richcontent>
+<font BOLD="true" NAME="Serif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1412177772072" ID="ID_1814234472" MODIFIED="1412177798680" STYLE="bubble" TEXT="&#x201c;&#x662f;&#x4ec0;&#x4e48;&#x201d;&#xff0c;&#x800c;&#x4e0d;&#x662f;&#x201c;&#x4e3a;&#x4ec0;&#x4e48;&#x201d;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1412177772075" ID="ID_846152951" MODIFIED="1412177798680" STYLE="bubble" TEXT="&#x6539;&#x53d8;&#xff0c;&#x4ece;&#x64cd;&#x4f5c;&#x65b9;&#x5f0f;&#x5f00;&#x59cb;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1412177772075" ID="ID_999916531" MODIFIED="1412177798680" STYLE="bubble" TEXT="&#x5927;&#x6570;&#x636e;&#xff0c;&#x6539;&#x53d8;&#x4eba;&#x7c7b;&#x63a2;&#x7d22;&#x4e16;&#x754c;&#x7684;&#x65b9;&#x6cd5;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1412177772075" ID="ID_426998680" MODIFIED="1412177798672" POSITION="right" STYLE="bubble" TEXT="&#x5546;&#x4e1a;&#x53d8;&#x9769;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font BOLD="true" NAME="Serif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1412177772075" HGAP="26" ID="ID_1597550766" MODIFIED="1412177818312" STYLE="bubble" TEXT="&#x6570;&#x636e;&#x5316;" VSHIFT="-19">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="Serif" SIZE="16"/>
+<node BACKGROUND_COLOR="#eeee00" COLOR="#990000" CREATED="1412177772075" ID="ID_842422279" MODIFIED="1412177798669" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>&#x4e00;&#x5207;&#x7686;&#x53ef;&#x201c;&#x91cf;&#x5316;&#x201d;</p></body>
+</html></richcontent>
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1412177772076" ID="ID_1490266013" MODIFIED="1412177798670" STYLE="bubble" TEXT="&#x6570;&#x636e;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1412177772076" ID="ID_734935002" MODIFIED="1412177798670" STYLE="bubble" TEXT="&#x4ece;&#x6700;&#x4e0d;&#x53ef;&#x80fd;&#x7684;&#x5730;&#x65b9;&#x63d0;&#x53d6;">
+<font BOLD="true" NAME="Serif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1412177772076" ID="ID_1482273323" MODIFIED="1412177798671" STYLE="bubble" TEXT="&#x6587;&#x5b57;">
+<font BOLD="true" NAME="Serif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1412177772076" ID="ID_376944296" MODIFIED="1412177798671" STYLE="bubble" TEXT="&#x65b9;&#x4f4d;">
+<font BOLD="true" NAME="Serif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1412177772076" ID="ID_272228411" MODIFIED="1412177798671" STYLE="bubble" TEXT="&#x6c9f;&#x901a;">
+<font BOLD="true" NAME="Serif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1412177772076" ID="ID_1725521655" MODIFIED="1412177798671" STYLE="bubble" TEXT="&#x6570;&#x636e;&#x5316;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1412177772076" ID="ID_1540783242" MODIFIED="1412177798671" STYLE="bubble" TEXT="&#x4e0d;&#x662f;&#x6570;&#x5b57;&#x5316;">
+<font BOLD="true" NAME="Serif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1412177772077" ID="ID_1446463509" MODIFIED="1412177798671" STYLE="bubble" TEXT="&#x91cf;&#x5316;&#x4e00;&#x5207;&#xff0c;&#x4e16;&#x95f4;&#x4e07;&#x7269;">
+<font BOLD="true" NAME="Serif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1412177772077" ID="ID_750492041" MODIFIED="1412177798655" STYLE="bubble" TEXT="&#x6570;&#x636e;&#x521b;&#x65b0;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="Serif" SIZE="16"/>
+<node BACKGROUND_COLOR="#eeee00" COLOR="#990000" CREATED="1412177772077" ID="ID_101276763" MODIFIED="1412177798656" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>&#x4ef7;&#x503c;&#xff1a;&#x201c;&#x53d6;&#x4e4b;&#x4e0d;&#x5c3d;&#xff0c;&#x7528;&#x4e4b;&#x4e0d;&#x7aed;&#x201d;&#x7684;&#x6570;&#x636e;&#x521b;&#x65b0;</p></body>
+</html></richcontent>
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1412177772082" ID="ID_1582469037" MODIFIED="1412177798657" STYLE="bubble" TEXT="&#x6570;&#x636e;&#x518d;&#x5229;&#x7528;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1412177772084" ID="ID_1188346785" MODIFIED="1412177798657" STYLE="bubble" TEXT="&#x91cd;&#x7ec4;&#x6570;&#x636e;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1412177772084" ID="ID_1524519491" MODIFIED="1412177798657" STYLE="bubble" TEXT="&#x53ef;&#x6269;&#x5c55;&#x6570;&#x636e;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1412177772085" ID="ID_1966153705" MODIFIED="1412177798657" STYLE="bubble" TEXT="&#x6570;&#x636e;&#x7684;&#x6298;&#x65e7;&#x503c;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1412177772085" ID="ID_219185931" MODIFIED="1412177798657" STYLE="bubble" TEXT="&#x6570;&#x636e;&#x5e9f;&#x6c14;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1412177772085" ID="ID_826004363" MODIFIED="1412177798658" STYLE="bubble" TEXT="&#x5f00;&#x653e;&#x6570;&#x636e;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1412177772085" ID="ID_1344833081" MODIFIED="1412177798658" STYLE="bubble" TEXT="&#x4f30;&#x503c;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1412177772085" HGAP="22" ID="ID_811238336" MODIFIED="1412177823440" STYLE="bubble" TEXT="&#x89d2;&#x8272;&#x5b9a;&#x4f4d;" VSHIFT="23">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="Serif" SIZE="16"/>
+<node BACKGROUND_COLOR="#eeee00" COLOR="#990000" CREATED="1412177772085" ID="ID_882961199" MODIFIED="1412177798647" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>&#x4e09;&#x8db3;&#x9f0e;&#x7acb;&#xff1a;&#x6570;&#x636e;&#xff0c;&#x6280;&#x672f;&#xff0c;&#x601d;&#x7ef4;</p></body>
+</html></richcontent>
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1412177772086" ID="ID_1401612403" MODIFIED="1412177798648" STYLE="bubble" TEXT="&#x5927;&#x6570;&#x636e;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1412177772086" ID="ID_1979474367" MODIFIED="1412177798649" STYLE="bubble" TEXT="&#x673a;&#x6784;">
+<font BOLD="true" NAME="Serif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1412177772086" ID="ID_578479483" MODIFIED="1412177798649" STYLE="bubble" TEXT="&#x638c;&#x63a7;&#x516c;&#x53f8;">
+<font BOLD="true" NAME="Serif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1412177772086" ID="ID_183570568" MODIFIED="1412177798649" STYLE="bubble" TEXT="&#x6280;&#x672f;&#x516c;&#x53f8;">
+<font BOLD="true" NAME="Serif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1412177772086" ID="ID_1338521950" MODIFIED="1412177798649" STYLE="bubble" TEXT="&#x5927;&#x6570;&#x636e;&#x601d;&#x7ef4;&#x516c;&#x53f8;&#x548c;&#x4e2a;&#x4eba;">
+<font BOLD="true" NAME="Serif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1412177772086" ID="ID_1887739533" MODIFIED="1412177798650" STYLE="bubble" TEXT="&#x6570;&#x636e;&#x4e2d;&#x95f4;&#x5546;">
+<font BOLD="true" NAME="Serif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1412177772086" ID="ID_196152072" MODIFIED="1412177798650" STYLE="bubble" TEXT="&#x4e13;&#x5bb6;&#x7684;&#x6d88;&#x4ea1;&#x4e0e;&#x6570;&#x636e;&#x79d1;&#x5b66;&#x5bb6;&#x7684;&#x5d1b;&#x8d77;">
+<font BOLD="true" NAME="Serif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1412177772087" ID="ID_1624377549" MODIFIED="1412177798650" STYLE="bubble" TEXT="&#x51b3;&#x5b9a;&#x4f01;&#x4e1a;&#x7ade;&#x4e89;&#x529b;">
+<font BOLD="true" NAME="Serif" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1412177772087" HGAP="30" ID="ID_1172050998" MODIFIED="1412177814210" POSITION="right" STYLE="bubble" TEXT="&#x7ba1;&#x7406;&#x53d8;&#x9769;" VSHIFT="44">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font BOLD="true" NAME="Serif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1412177772087" ID="ID_895697559" MODIFIED="1412177798630" STYLE="bubble" TEXT="&#x98ce;&#x9669;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="Serif" SIZE="16"/>
+<node BACKGROUND_COLOR="#eeee00" COLOR="#990000" CREATED="1412177772087" ID="ID_848805131" MODIFIED="1412177798631" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>&#x8ba9;&#x6570;&#x636e;&#x4e3b;&#x5bb0;&#x4e00;&#x5207;&#x7684;&#x9690;&#x5fe7;</p></body>
+</html></richcontent>
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1412177772087" ID="ID_392683566" MODIFIED="1412177798632" STYLE="bubble" TEXT="&#x65e0;&#x5904;&#x4e0d;&#x5728;&#x7684;&#x201c;&#x7b2c;&#x4e09;&#x53ea;&#x773c;&#x201d;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1412177772088" ID="ID_826512963" MODIFIED="1412177798632" STYLE="bubble" TEXT="&#x9690;&#x79c1;&#x88ab;&#x4e8c;&#x6b21;&#x5229;&#x7528;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1412177772088" ID="ID_296898479" MODIFIED="1412177798632" STYLE="bubble" TEXT="&#x9884;&#x6d4b;&#x4e0e;&#x60e9;&#x7f5a;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+<node BACKGROUND_COLOR="#eeee00" COLOR="#111111" CREATED="1412177772088" ID="ID_1973290572" MODIFIED="1412177798633" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>&#x4e0d;&#x662f;&#x56e0;&#x4e3a;&#x201c;&#x6240;&#x505a;&#x201d;&#xff0c;&#x800c;&#x662f;&#x56e0;&#x4e3a;&#x201c;&#x5c06;&#x505a;&#x201d;</p></body>
+</html></richcontent>
+<font BOLD="true" NAME="Serif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1412177772088" ID="ID_854791652" MODIFIED="1412177798633" STYLE="bubble" TEXT="&#x6570;&#x636e;&#x72ec;&#x88c1;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1412177772088" ID="ID_1771657708" MODIFIED="1412177798633" STYLE="bubble" TEXT="&#x6323;&#x8131;&#x5927;&#x6570;&#x636e;&#x7684;&#x56f0;&#x5883;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1412177772089" ID="ID_355260250" MODIFIED="1412177827840" STYLE="bubble" TEXT="&#x638c;&#x63a7;" VSHIFT="33">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="Serif" SIZE="16"/>
+<node BACKGROUND_COLOR="#eeee00" COLOR="#990000" CREATED="1412177772089" ID="ID_385526501" MODIFIED="1412177798623" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>&#x8d23;&#x4efb;&#x4e0e;&#x81ea;&#x7531;&#x5e76;&#x4e3e;&#x7684;&#x4fe1;&#x606f;&#x7ba1;&#x7406;</p></body>
+</html></richcontent>
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1412177772089" ID="ID_1114999839" MODIFIED="1412177798624" STYLE="bubble" TEXT="&#x4e2a;&#x4eba;&#x9690;&#x79c1;&#x4fdd;&#x62a4;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+<node BACKGROUND_COLOR="#eeee00" COLOR="#111111" CREATED="1412177772093" ID="ID_268352882" MODIFIED="1412177798624" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>&#x4ece;&#x4e2a;&#x4eba;&#x8bb8;&#x53ef;&#x8bc1;&#x5230;&#x8ba9;&#x6570;&#x636e;&#x4f7f;&#x7528;&#x8005;&#x627f;&#x62c5;&#x8d23;&#x4efb;</p></body>
+</html></richcontent>
+<font BOLD="true" NAME="Serif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1412177772095" ID="ID_393747841" MODIFIED="1412177798625" STYLE="bubble" TEXT="&#x4e2a;&#x4eba;&#x52a8;&#x56e0;vs&#x9884;&#x6d4b;&#x5206;&#x6790;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1412177772095" ID="ID_1627760221" MODIFIED="1412177798625" STYLE="bubble" TEXT="&#x51fb;&#x788e;&#x9ed1;&#x76d2;&#x5b50;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+<node BACKGROUND_COLOR="#eeee00" COLOR="#111111" CREATED="1412177772095" ID="ID_659352348" MODIFIED="1412177798626" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+<head>
+<style type="text/css">
+<!--
+p { margin-top: 3px; margin-bottom: 3px; }-->
+</style>
+</head>
+<body>
+<p>&#x5927;&#x6570;&#x636e;&#x7b97;&#x6cd5;&#x5e08;&#x7684;&#x5d1b;&#x8d77;</p></body>
+</html></richcontent>
+<font BOLD="true" NAME="Serif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1412177772096" ID="ID_81659218" MODIFIED="1412177798626" STYLE="bubble" TEXT="&#x53cd;&#x6570;&#x636e;&#x5784;&#x65ad;&#x5927;&#x4ea8;">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
