54e5107243b1006903c594e75fea83416497d834
antinomy <antinomy.net@126.com>
2014-10-14 23:58:17 +0800

    2014-10-14-The Little Mocker

---
+++ Test/Mock/Mocks.mm
@@ -0,0 +1,25 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1413301929443" ID="ID_1068464208" MODIFIED="1413302034586" STYLE="bubble" TEXT="Test Doubles">
+<font BOLD="true" NAME="Serif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1413301929443" HGAP="21" ID="ID_1726358025" MODIFIED="1413302054004" POSITION="right" STYLE="bubble" TEXT="Dummy" VSHIFT="-43">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font BOLD="true" NAME="Serif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1413301929443" HGAP="24" ID="ID_646652253" MODIFIED="1413302034583" STYLE="bubble" TEXT="Stub" VSHIFT="-35">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="Serif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1413301929443" ID="ID_1634475259" MODIFIED="1413302034584" STYLE="bubble" TEXT="Spy" VSHIFT="-15">
+<font BOLD="true" NAME="Serif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1413301929444" HGAP="25" ID="ID_528584301" MODIFIED="1413302034584" STYLE="bubble" TEXT="Mock" VSHIFT="-18">
+<font BOLD="true" NAME="Serif" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1413301929444" HGAP="29" ID="ID_1086307214" MODIFIED="1413302034578" POSITION="right" STYLE="bubble" TEXT="Fake" VSHIFT="59">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font BOLD="true" NAME="Serif" SIZE="18"/>
+</node>
+</node>
+</map>
