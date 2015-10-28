fa06a2507f849af63301d32254eea319b4923c30
Antinomy <antinomy.net@126.com>
2015-10-28 10:53:10 +0800

    更新的内容 :
            save doc

---
+++ Yoga/Ashtanga.mm
@@ -0,0 +1,215 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1445742905393" ID="ID_431498303" MODIFIED="1445743168002" TEXT="&#x7b2c;&#x4e00;&#x5e8f;&#x5217; &#x2022; The Primary Series">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1445742905394" FOLDED="true" HGAP="59" ID="ID_1517446423" MODIFIED="1445743167998" POSITION="right" TEXT="&#x70ed;&#x8eab;" VSHIFT="-51">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1445742905394" ID="ID_1268617458" MODIFIED="1445742956095" TEXT="&#x62dc;&#x65e5;&#x5f0f; [A-B]  &#x2022; Surya Namaskara &#x2022;  Sun Salutation">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1445742905394" FOLDED="true" HGAP="69" ID="ID_40976509" MODIFIED="1445743167999" POSITION="right" TEXT="&#x7ad9;&#x7acb;&#x4f53;&#x52bf;" VSHIFT="-84">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1445742905394" ID="ID_1856552027" MODIFIED="1445742956100" TEXT="1. &#x53cc;&#x817f;&#x524d;&#x5c48;&#x52a0;&#x5f3a;&#x80cc;&#x90e8;&#x4f38;&#x5c55;&#x5f0f;A (&#x9e35;&#x9e1f;&#x5f0f;) &#x2022; Padangusth Asana &#x2022; Big Toe Posture">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905394" ID="ID_155260577" MODIFIED="1445742956110" TEXT="2. &#x53cc;&#x817f;&#x524d;&#x5c48;&#x52a0;&#x5f3a;&#x80cc;&#x90e8;&#x4f38;&#x5c55;&#x5f0f;B (&#x524d;&#x5c48;&#x5f0f;)  &#x2022; Padahast Asana &#x2022; Hand Under Foot Posture">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905394" ID="ID_1355811478" MODIFIED="1445742956117" TEXT="3. &#x4e09;&#x89d2;&#x5f0f; &#x2022; Utthita Trikon Asana &#x2022; Triangle Posture">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905394" ID="ID_1179237386" MODIFIED="1445742956119" TEXT="4. &#x626d;&#x8f6c;&#x4e09;&#x89d2;&#x5f0f; &#x2022; Parivrtta Trikon Asana &#x2022; Revolving Triangle">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905394" ID="ID_1111859457" MODIFIED="1445742956120" TEXT="5. &#x4fa7;&#x89d2;&#x5f0f; &#x2022; Utthita Parsvakon Asana &#x2022; Extended Side Angle Pose">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905394" ID="ID_1823591316" MODIFIED="1445742956121" TEXT="6. &#x626d;&#x8f6c;&#x4fa7;&#x89d2;&#x5f0f; &#x2022;  Parivritta Parsvakon Asana &#x2022; Revolving Side Angle Pose">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905394" ID="ID_754933106" MODIFIED="1445742956131" TEXT="7. &#x53cc;&#x89d2;&#x5f0f; [A-B-C-D] &#x2022; Prasarita Padottan Asana A-D &#x2022; Intense Wide Leg Stretch">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905395" ID="ID_425815548" MODIFIED="1445742956135" TEXT="8. &#x5355;&#x817f;&#x52a0;&#x5f3a;&#x80cc;&#x90e8;&#x4f38;&#x5c55;&#x5f0f; &#x2022; Arsvottan Asana &#x2022; Intense Side Stretch Posture">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905395" ID="ID_905879768" MODIFIED="1445742956145" TEXT="9. &#x624b;&#x62c9;&#x811a;&#x5355;&#x817f;&#x76f4;&#x7acb;&#x5f0f; [A-B-C-D] &#x2022; Utthita Hasta Padangusth Asana &#x2022; Hand To Big Toe Posture">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905395" ID="ID_781940027" MODIFIED="1445742956159" TEXT="10. &#x534a;&#x83b2;&#x82b1;&#x5355;&#x817f;&#x52a0;&#x5f3a;&#x80cc;&#x90e8;&#x4f38;&#x5c55;&#x5f0f; &#x2022; Ardha Baddha Padmottan Asana &#x2022; Half Bound Lotus Forward Bend">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905395" ID="ID_409965041" MODIFIED="1445742956166" TEXT="11. &#x5e7b;&#x6905;&#x5f0f; &#x2022; Utkat Asana &#x2022; Fierce Pose">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905395" ID="ID_84351829" MODIFIED="1445742956168" TEXT="12. &#x6218;&#x58eb;&#x5f0f; [A-B] &#x2022; VIRABHADR ASANA A-B &#x2022; WARRIOR">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1445742905395" FOLDED="true" HGAP="104" ID="ID_204482856" MODIFIED="1445743181192" POSITION="right" TEXT="&#x5750;&#x7acb;&#x4f53;&#x52bf;" VSHIFT="-36">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1445742905395" ID="ID_1180733954" MODIFIED="1445742956172" TEXT="13. &#x5750;&#x5c71;&#x5f0f; &#x2022; Dand Asana &#x2022; Staff Pose">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905395" ID="ID_1185294573" MODIFIED="1445742956195" TEXT="14. &#x53cc;&#x817f;&#x52a0;&#x5f3a;&#x80cc;&#x90e8;&#x4f38;&#x5c55;&#x5f0f; [A-B-C] &#x2022; Paschimottan Asana A-C &#x2022; Intense West Stretch Or Sitting Forward Bend">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905395" ID="ID_807868507" MODIFIED="1445742956201" TEXT="15. &#x53cd;&#x53f0;&#x5f0f; &#x2022; Purvottan Asana &#x2022; Intense East Stretch">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905395" ID="ID_73270332" MODIFIED="1445742956217" TEXT="16. &#x534a;&#x83b2;&#x82b1;&#x524d;&#x5c48;&#x52a0;&#x5f3a;&#x80cc;&#x90e8;&#x4f38;&#x5c55;&#x5f0f; &#x2022; Ardha Baddha Padma Paschimott Anasana &#x2022; Half Bound Lotus Forward Bend">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905396" ID="ID_279120003" MODIFIED="1445742956232" TEXT="17. &#x534a;&#x82f1;&#x96c4;&#x524d;&#x5c48;&#x52a0;&#x5f3a;&#x80cc;&#x90e8;&#x4f38;&#x5c55;&#x5f0f; &#x2022; Iriang Mukhaipada Paschimottan Asana &#x2022; Reverse The Leg Direction Of One Leg Intense">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905396" ID="ID_328456783" MODIFIED="1445742956240" TEXT="18. &#x5355;&#x817f;&#x524d;&#x5c48;&#x52a0;&#x5f3a;&#x80cc;&#x90e8;&#x4f38;&#x5c55;&#x5f0f; [A-B-C] &#x2022; Janu Sirs Asana A-C &#x2022; Head To Knee Pose">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905396" ID="ID_1502081021" MODIFIED="1445742956244" TEXT="19. &#x739b;&#x91cc;&#x742a; [A-B-C-D] &#x2022; Marichy Asana A-D">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905396" ID="ID_763204211" MODIFIED="1445742956245" TEXT="20. &#x8239;&#x5f0f; &#x2022; Nav Asana &#x2022; Boat Posture">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905396" ID="ID_1144123239" MODIFIED="1445742956246" TEXT="21. &#x538b;&#x80a9;&#x5f0f; [A-B-C-D] &#x2022; Bhujapid Asana &#x2022; Shoulder Pressure Posture">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905396" ID="ID_1216129195" MODIFIED="1445742956248" TEXT="22. &#x9f9f;&#x5f0f; &#x2022; Kurm Asana &#x2022; Tortoise Posture">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905396" ID="ID_789401455" MODIFIED="1445742956249" TEXT="23. &#x7761;&#x9f9f;&#x5f0f; &#x2022; Supta Kurm Asana &#x2022; Sleeping Tortoise">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905396" ID="ID_1009739357" MODIFIED="1445742956250" TEXT="24. &#x80ce;&#x513f;&#x5f0f; &#x2022; Garbha Pind Asana &#x2022; Embryo Posture">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905396" ID="ID_1116872435" MODIFIED="1445742956251" TEXT="25. &#x516c;&#x9e21;&#x5f0f; &#x2022; Kukkut Asana &#x2022; Rooster Posture">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905396" ID="ID_1808662006" MODIFIED="1445742956253" TEXT="26. &#x675f;&#x89d2;&#x5f0f; [A-B] &#x2022; Baddha Kon Asana A-B &#x2022; Bound Angle Posture">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905397" ID="ID_1963151711" MODIFIED="1445742956254" TEXT="27. &#x5750;&#x89d2;&#x5f0f; [A-B] &#x2022; Upavista Kon Asana A-B &#x2022; Seated Angle Posture">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905397" ID="ID_427111680" MODIFIED="1445742956255" TEXT="28. &#x7761;&#x89d2;&#x5f0f; [A-B] &#x2022; Supta Kon Asana A-B &#x2022; Lying-Down Angle Posture">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905397" ID="ID_26213510" MODIFIED="1445742956262" TEXT="29. &#x4ef0;&#x5367;&#x624b;&#x62c9;&#x811a;&#x6307;&#x4f38;&#x5c55;&#x5f0f; [A-C] &#x2022; Supta Padangust Asana &#x2022; Reclining Big Toe Posture">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905397" ID="ID_148970970" MODIFIED="1445742956265" TEXT="30. &#x5168;&#x7bad;&#x5f0f; [A-B] &#x2022; Ubhaya Padangusth Asana &#x2022; Both Big Toes Posture">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905397" ID="ID_1033767487" MODIFIED="1445742956270" TEXT="31. &#x4ef0;&#x5367;&#x80cc;&#x817f;&#x4f38;&#x5c55;&#x5f0f; [A-B] &#x2022; Urdhva Mukha Paschimott Asana &#x2022; Upward Facing Full Forward Bend">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1445742905397" FOLDED="true" HGAP="104" ID="ID_329924805" MODIFIED="1445743183678" POSITION="right" TEXT="&#x540e;&#x5f2f;&#x4f53;&#x52bf;" VSHIFT="6">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1445742905397" ID="ID_281754687" MODIFIED="1445742956275" TEXT="32. &#x6865;&#x5f0f; &#x2022; Setu Bandh Asana &#x2022; Bridge">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905397" ID="ID_432808890" MODIFIED="1445742956276" TEXT="33. &#x8f6e;&#x5f0f; &#x2022; Urdhva Dhanur Asana &#x2022; Upward Bow Posture">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905397" ID="ID_698107841" MODIFIED="1445742956278" TEXT="34. &#x5750;&#x5f0f;&#x524d;&#x5c48; &#x2022; Paschimottan Asana &#x2022; Full Forward Bend">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905397" ID="ID_1961314562" MODIFIED="1445742956279" TEXT="35. &#x80a9;&#x5012;&#x7acb; &#x2022; Salamba Sarvang Asana &#x2022; Shoulderstand">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905398" ID="ID_1790040763" MODIFIED="1445742956280" TEXT="36. &#x7281;&#x5f0f; &#x2022; Hal Asana &#x2022; Plow">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905398" ID="ID_1732356133" MODIFIED="1445742956281" TEXT="37. &#x8033;&#x6735;&#x5f0f; &#x2022; Karnapid Asana &#x2022; Ear Pressure Posture">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905398" ID="ID_322023675" MODIFIED="1445742956283" TEXT="38. &#x5012;&#x7acb;&#x83b2;&#x82b1;&#x5f0f; &#x2022; Urdhva Padm Asana &#x2022; Upward Lotus">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905398" ID="ID_1350482601" MODIFIED="1445742956284" TEXT="39. &#x624b;&#x62b1;&#x83b2;&#x82b1;&#x817f;&#x80a9;&#x5012;&#x7acb; &#x2022; Pind Asana &#x2022; Embryo Posture">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905398" ID="ID_101365788" MODIFIED="1445742956285" TEXT="40. &#x9c7c;&#x5f0f; &#x2022; Matsy Asana &#x2022; Fish Posture">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905398" ID="ID_1046866553" MODIFIED="1445742956286" TEXT="41. &#x5b8c;&#x5168;&#x9c7c;&#x5f0f; &#x2022; Uttana Padasana &#x2022; Extended Leg Posture">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1445742905398" FOLDED="true" HGAP="69" ID="ID_3941477" MODIFIED="1445743168002" POSITION="right" TEXT="&#x7ed3;&#x675f;&#x4f53;&#x4f4d;" VSHIFT="52">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1445742905398" ID="ID_1662824838" MODIFIED="1445742956289" TEXT="42. &#x5934;&#x5012;&#x7acb; &#x2022; Sirs Asana &#x2022; Headstand">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905398" ID="ID_27325246" MODIFIED="1445742956290" TEXT="43. &#x6346;&#x7ed1;&#x83b2;&#x82b1;&#x524d;&#x66f2;&#x4f38;&#x5c55;&#x5f0f; &#x2022; Yoga Mudra &#x2022; Sealed Yoga Posture">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905398" ID="ID_159958936" MODIFIED="1445742956291" TEXT="44. &#x83b2;&#x82b1;&#x5750;&#x7acb;&#x5f0f; &#x2022; Padm Asana &#x2022; Lotus">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905398" ID="ID_388863201" MODIFIED="1445742956292" TEXT="45. &#x83b2;&#x82b1;&#x652f;&#x6491; &#x2022; Uttpluthi &#x2022; Uprooting">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1445742905398" ID="ID_1721730892" MODIFIED="1445742956295" TEXT="46. &#x644a;&#x5c38;&#x5f0f; &#x2022; Sav Asana &#x2022; Corpse Posture">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
